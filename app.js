2ca31455db006901648538bf6075875b25079f2c
yinyong <yinyong@sogou-inc.com>
2014-10-28 20:10:57 +0800

    增加新的SEM模板

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -40,7 +39,6 @@ app.use('*', function(req, res, next) {
 
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
